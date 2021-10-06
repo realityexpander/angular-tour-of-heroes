96025905de7f3d59cba84961b565eab5b1a97996
Chris Athanas <chris@crowdskout.com>
2021-10-05 22:57:36 -0500

    Convert getHeroes to Observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,7 +11,8 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 }
